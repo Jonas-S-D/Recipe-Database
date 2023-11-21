f8f33352dae3f49ece8eee996f7ef5c9e8ded241
Jonas-S-D <144025767+Jonas-S-D@users.noreply.github.com>
2023-11-21 15:44:56 +0100

    User Input for ingredients or categories.

--- src/library/app-library.h
+++ src/library/app-library.h
@@ -1,2 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+
+#define MAX_NAME_LENGTH 50
+#define MAX_INGREDIENT_LENGTH 30
+
+void getUserInput(char ***ingredients, int *ingredientCount, char ***categories, int *categoryCount);
+void getUserInputIngredients(char ***ingredients, int *ingredientCount);
+void getUserInputCategories(char ***categories, int *categoryCount);
+void freeMemory(char ***array, int count);
