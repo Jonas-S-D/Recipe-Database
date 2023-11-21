4e2b41fd65acddd4dd84e02618470c87a3132f9a
Jonas-S-D <144025767+Jonas-S-D@users.noreply.github.com>
2023-11-21 15:44:56 +0100

    User Input for ingredients or categories.

--- src/app.c
+++ src/app.c
@@ -1,6 +1,26 @@
 #include "library/app-library.c"
 
 int main() {
+    // User input variables
+    char **userIngredients = NULL;
+    int userIngredientCount = 0;
+    char **categories = NULL;
+    int categoryCount = 0;
+
+    // Step 1: Program Explanation
+
+    // Step 2: Get user input
+    getUserInput(&userIngredients, &userIngredientCount, &categories, &categoryCount);
+
+    // Step 3: Display dishes based on user input
+
+    // Step 4: Let the user pick their dish
+
+    // Step 5: Display the recipe.
+
+    // Free allocated memory for ingredients and categories
+    freeMemory(&userIngredients, userIngredientCount);
+    freeMemory(&categories, categoryCount);
 
     return 0;
 }
